f7688fcb442e174890090d0d4643dfdd7ba27d60
Keith Marchant <kemarchant@outlook.com>
2022-01-13 20:44:10 +1100

    Adding project to git

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
+
+const PORT = process.env.POST || 5000;
+
+const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 5
+});
+app.use(limiter);
+app.set('trust-proxy', 1);
+
+// Routes
+app.use('/api', require('./routes'));
+
+// Enable CORS
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
